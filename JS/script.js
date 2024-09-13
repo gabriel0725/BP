e373e38038274afe62a6c90211daad48363fa105
Clark <galaxy.games199@gmail.com>
2024-09-12 21:27:35 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const links = document.querySelectorAll(".carousel-link");
+
+	function updateLinks() {
+		links.forEach(link => {
+			const radioId = link.parentElement.getAttribute("for");
+			const radio = document.getElementById(radioId);
+			
+			if (radio.checked) {
+				link.classList.add("active-link");
+			} else {
+				link.classList.remove("active-link");
+			}
+		});
+	}
+
+	links.forEach(link => {
+		link.addEventListener("click", function(event) {
+			if (!this.classList.contains("active-link")) {
+				event.preventDefault();
+			}
+		});
+	});
+
+	const radios = document.querySelectorAll('input[name="slider"]');
+	radios.forEach(radio => {
+		radio.addEventListener("change", updateLinks);
+	});
+
+	updateLinks();
+});
