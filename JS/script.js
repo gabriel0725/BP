5e11b8d65d2d6e4c229dcb73c3e7047717c8c0ce
Clark <galaxy.games199@gmail.com>
2024-09-25 18:12:18 -0300

    ajuste carrossel

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const links = document.querySelectorAll(".carousel-link");
+    const links = document.querySelectorAll(".carousel-link");
 
-	function updateLinks() {
-		links.forEach(link => {
-			const radioId = link.parentElement.getAttribute("for");
-			const radio = document.getElementById(radioId);
-			
-			if (radio.checked) {
-				link.classList.add("active-link");
-			} else {
-				link.classList.remove("active-link");
-			}
-		});
-	}
+    function updateLinks() {
+        links.forEach(link => {
+            const radioId = link.parentElement.getAttribute("for");
+            const radio = document.getElementById(radioId);
+            
+            if (radio.checked) {
+                link.classList.add("active-link");
+            } else {
+                link.classList.remove("active-link");
+            }
+        });
+    }
 
-	links.forEach(link => {
-		link.addEventListener("click", function(event) {
-			if (!this.classList.contains("active-link")) {
-				event.preventDefault();
-			}
-		});
-	});
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            if (!this.classList.contains("active-link")) {
+                event.preventDefault();
+            }
+        });
+    });
 
-	const radios = document.querySelectorAll('input[name="slider"]');
-	radios.forEach(radio => {
-		radio.addEventListener("change", updateLinks);
-	});
+    const radios = document.querySelectorAll('input[name="slider"]');
+    radios.forEach(radio => {
+        radio.addEventListener("change", updateLinks);
+    });
 
-	updateLinks();
+    updateLinks();
 });
-
-
-
